e33116e7e7b0e166dd8b1e89a8618b57dea06c7c
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-08-08 15:30:04 +0530

    1

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 let x = document.querySelector(".btn");
 let y = document.querySelector(".ok");
 
-console.log(x.value);
-
 document.querySelector(".calculator").addEventListener("click", function () {
 	y.innerHTML = x.value;
-	console.log(x.value);
+	// console.log(x.value);
 });

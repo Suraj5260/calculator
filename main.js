1fee5fd3a3df1134611f33eb082972d09faaf179
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-08-08 13:49:33 +0530

    added bg display

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let x = document.getElementById("disp");
-var y = document.getElementById("ttl");
+var x = document.querySelector(".btn");
+var y = document.querySelector(".ok");
 
-console.log(x);
-console.log(y);
+console.log(x.value);
+
+document.querySelector(".calculator").addEventListener("click", function () {
+	y.innerHTML = x.value;
+	console.log(x.value);
+});
